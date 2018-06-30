a67133392405601e91029337261be21e50c1c72e
Shunichiro Ida <shunichiroid@gmail.com>
2018-06-30 20:48:00 +1000

    Create views

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const ig = require('instagram-node').instagram()
 
 const app = express()
+
+app.use(express.static(__dirname + '/public'))
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('pages/index')
+})
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
 app.listen(8080)
-
 console.log('Server has started');
