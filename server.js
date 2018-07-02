c0fd192012b76c3747ffa615ba72732d3d20930a
Shunichiro Ida <shunichiroid@gmail.com>
2018-07-02 11:08:03 +1000

    Modify index page

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express')
 const ig = require('instagram-node').instagram()
-
 const app = express()
 
+require('dotenv').config();
+
 app.use(express.static(__dirname + '/public'))
+ig.use({
+    access_token: process.env.INSTAGRAM_TOKEN
+})
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('pages/index')
-})
+    ig.user_self_media_recent((err, medias, pagination, remaining, limit) => {
+        res.render('pages/index', { grams: medias })
+    })
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
 })
 
 app.listen(8080)
